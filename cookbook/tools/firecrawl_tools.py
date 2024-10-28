f99a3622fdaebc4888cc2025895ae7a2a197f8f9
Abhishek Ranjan <aviranjan444@gmail.com>
2024-10-28 09:11:00 +0530

    feat: add map, crawl status check and other methods in the existing Firecrawl toolkit

--- cookbook/tools/firecrawl_tools.py
+++ cookbook/tools/firecrawl_tools.py
@@ -1,5 +1,45 @@
 from phi.agent import Agent
 from phi.tools.firecrawl import FirecrawlTools
+from dotenv import load_dotenv
 
-agent = Agent(tools=[FirecrawlTools(scrape=False, crawl=True)], show_tool_calls=True, markdown=True)
-agent.print_response("Summarize this https://finance.yahoo.com/")
+load_dotenv()
+
+
+def agent_with_predefined_toolkit(url: str):
+    agent = Agent(
+        tools=[FirecrawlTools(map=True, scrape=True)],
+        show_tool_calls=True,
+        markdown=True,
+        reasoning=True,
+    )
+    agent.print_response(f"Fetch all the blog links from the website: {url}")
+    agent.print_response(
+        "scrape and summarize the following website: https://www.firecrawl.dev/blog/using-structured-output-and-json-strict-mode-openai"
+    )
+
+
+# def agent_with_explicit_tools(url: str):
+#     fc_tools = [
+#         FirecrawlTools(map=True, scrape=True).map_website(url, include_subdomains=True, ignore_sitemap=False, limit=10),
+#         FirecrawlTools(map=True, scrape=True).scrape_website(url),
+#     ]
+#     agent = Agent(tools=fc_tools, show_tool_calls=True, markdown=True, reasoning=True)
+#     agent.print_response(f"Fetch all the blog links from the website: {url}")
+#     agent.print_response(
+#         "scrape and summarize the following website: https://www.firecrawl.dev/blog/using-structured-output-and-json-strict-mode-openai"
+#     )
+
+
+def crawl_example(url: str):
+    agent = Agent(
+        tools=[FirecrawlTools(scrape=True, crawl=True, async_crawl=True, crawl_limit=25)],
+        show_tool_calls=True,
+        reasoning=True,
+        markdown=True,
+    )
+
+
+if __name__ == "__main__":
+    url = "https://www.firecrawl.dev/blog"
+    # agent_with_explicit_tools(url)
+    agent_with_predefined_toolkit(url)
